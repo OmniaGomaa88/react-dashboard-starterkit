f0b32b37288d4ac12c157a0bf2d3e8fdf1a58740
OmniaGomaa88 <minna1172018@gmail.com>
2021-06-29 19:46:37 +0200

    connect formateurs

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,48 +1,79 @@
 import React from "react";
-import { useRef, useEffect, useState,useContext } from "react";
-import promoService from '../../services/promo'
+import { useRef, useEffect, useState, useContext } from "react";
+import promoService from "../../services/promo";
+import formateurService from "../../services/formateur";
+import apprenantService from "../../services/apprenant"
+
 const Dashboard = (props) => {
   const [promos, setPromos] = useState([]);
+  const [Formateurs, setFormateurs] = useState([]);
+  const [apprenants,setApprenant]=useState([])
   const [error, setError] = useState(false);
   const isInitialMount = useRef(true);
 
+  // function de get Promos
+  const getPromos = async () => {
+    try {
+      const promosdata = await promoService.getAll();
+      setPromos(promosdata.data);
+      
+    } catch (error) {
+      setError(true);
+    }
+  };
+  // function pour get formteur
+  const getFormateurs = async () => {
+    try {
+      const FormateursData = await formateurService.getAll();
+      setFormateurs(FormateursData.data);
+    console.log(FormateursData.data)
+    } catch (error) {
+      setError(true);
+    }
+  };
+   // function pour get apprenants
+   const getApprenants = async () => {
+    try {
+      const apprenantData = await apprenantService.getAll();
+      setFormateurs(apprenantData.data);
+    
+    } catch (error) {
+      setError(true);
+    }
+  };
+
+  useEffect((props) => {
+    getPromos();
+   
+    getApprenants();
+    
+  }, []);
+  useEffect((props)=>{
+    getFormateurs();
+  },[])
+
+  return (
+    <div>
+      {error && <p>{error}</p>}
+      <h1>les promos !</h1>
+
+      {promos.map((promo, index) => {
+        return <li key={index}>{promo.name}</li>;
+      })}
+    <div>
+      
+      <h1>les formateurs</h1>
+      {Formateurs.map((formateur, index) => {
+        return <li key={index}>{formateur.firstname}</li>;
+      })}
+   </div>
+    
+    </div>
+    
+    
+  );
   
-    const getPromos = async () => {
-      
-        const promosdata = await promoService.getAll();
-       
-        setPromos(promosdata.data);
-        
-        console.log(promos)
-        console.log(promosdata.data)
-      
-    };
-
-
-    useEffect((props)=>{
-        getPromos();
-      
-    },[])
-
- 
-    return (
-       
-        <div>
-          {error && <p>{error}</p>}
-          <h1>Welcome !</h1>
-
-          {promos.map((promo,index)=>{
-              return(
-               <li key={index}>{promo.name}</li>
-              )
-              
-          })
-        }
-          </div>
-       
-      );
-     
-
+  
 };
 
 export default Dashboard;
